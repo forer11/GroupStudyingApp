2cea9d9ea4f45c1c393e27fe1b5449c28bc6fd2c
forer11 <lior.forer@cs.huji.ac.il>
2020-06-20 17:58:19 +0300

    changed code to be clearer

--- app/src/main/java/com/example/groupstudyingapp/CoursePageActivity.java
+++ app/src/main/java/com/example/groupstudyingapp/CoursePageActivity.java
@@ -13,7 +13,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class CoursePageActivity extends AppCompatActivity implements CoursePageAdapter.ItemClickListener {
     public static final String IMAGE_UPLOADED = "image_uploaded";
@@ -47,13 +46,11 @@ public class CoursePageActivity extends AppCompatActivity implements CoursePageA
         questions = course.getQuestions();
         questions.sort(qCompare);
         setRecyclerView();
-        findViewById(R.id.addQuestionButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), AddQuestionActivity.class);
-                startActivity(intent);
-            }
-        });
+        setAddQuestionButtonListener();
+        setBroadcastReceiver();
+    }
+
+    private void setBroadcastReceiver() {
         br = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
@@ -67,6 +64,16 @@ public class CoursePageActivity extends AppCompatActivity implements CoursePageA
         };
         IntentFilter filter = new IntentFilter(IMAGE_UPLOADED);
         this.registerReceiver(br, filter);
+    }
+
+    private void setAddQuestionButtonListener() {
+        findViewById(R.id.addQuestionButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), AddQuestionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private String getNewCourseId() {
@@ -94,56 +101,6 @@ public class CoursePageActivity extends AppCompatActivity implements CoursePageA
         adapter.notifyDataSetChanged();
     }
 
-
-//    private void showInsertDialog() {
-//        final FlatDialog flatDialog = new FlatDialog(CoursePageActivity.this);
-//        flatDialog.setFirstTextField("");
-//        flatDialog.setTitle("Add a question")
-//                .setSubtitle("Write your question title here")
-//                .setFirstTextFieldHint("Question title")
-//                .setFirstButtonText("Camera")
-//                .setSecondButtonText("From Gallery")
-//                .setThirdButtonText("Done")
-//                .withFirstButtonListner(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        dispatchTakePictureIntent();
-//                    }
-//                })
-//                .withSecondButtonListner(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        Intent pickPhoto = new Intent(Intent.ACTION_PICK,
-//                                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//                        startActivityForResult(pickPhoto, GALLERY_ACTION);
-//                    }
-//                })
-//                .withThirdButtonListner(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View view) {
-//                        if (flatDialog.getFirstTextField().equals("")) {
-//                            Toast.makeText(CoursePageActivity.this, "Please write title", Toast.LENGTH_SHORT).show();
-//                        } else {
-//                            String questionTitleInput = flatDialog.getFirstTextField();
-//                            if (!isPhotoEntered) {
-//                                Toast.makeText(CoursePageActivity.this, PLS_UPLOAD_IMG, Toast.LENGTH_SHORT).show();
-//                            } else {
-//                                Question newQuestion = new Question(questionTitleInput,
-//                                        newQuestionImagePath);
-//                                fireStoreHandler.uploadQuestionImage(newImageUri,
-//                                        newQuestionImagePath,
-//                                        newQuestion,
-//                                        CoursePageActivity.this);
-//                                flatDialog.dismiss();
-//                            }
-//                        }
-//
-//                    }
-//                })
-//                .show();
-//        flatDialog.setCanceledOnTouchOutside(true);
-//    }
-
     private void setRecyclerView() {
         // set up the RecyclerView
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
@@ -164,6 +121,7 @@ public class CoursePageActivity extends AppCompatActivity implements CoursePageA
         startActivity(intent);
     }
 
+    //TODO Mor/Ido do we need those?
     private void insertSingleItem(Question newQuestion) {
         questions.add(newQuestion);
         adapter.notifyDataSetChanged();
