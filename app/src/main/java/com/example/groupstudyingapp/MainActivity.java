86ea098b47944a399faff2b0ba1e72eba045286f
amit-schwartz-u <57283327+amit-schwartz-u@users.noreply.github.com>
2020-06-23 12:47:00 +0300

    added: new request from app manager by mail - to add a new course.

--- app/src/main/java/com/example/groupstudyingapp/MainActivity.java
+++ app/src/main/java/com/example/groupstudyingapp/MainActivity.java
@@ -14,6 +14,8 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 
 public class MainActivity extends BaseMenuActivity implements CoursesAdapter.ItemClickListener {
@@ -32,6 +34,15 @@ public class MainActivity extends BaseMenuActivity implements CoursesAdapter.Ite
         setToolbar();
         getAppData();
         setRecyclerViews();
+
+        final FloatingActionButton addCourseButton = findViewById(R.id.addCourseButton);
+        addCourseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SendEmailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     // TODO: lior , delete before submission
