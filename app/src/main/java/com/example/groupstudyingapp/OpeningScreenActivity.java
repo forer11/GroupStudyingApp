ed6b4df56d0c77a7f9df71a7c6f47b5363d95082
forer11 <lior.forer@cs.huji.ac.il>
2020-06-27 00:40:41 +0300

    now splash ends when we finish loading data

--- app/src/main/java/com/example/groupstudyingapp/OpeningScreenActivity.java
+++ app/src/main/java/com/example/groupstudyingapp/OpeningScreenActivity.java
@@ -2,52 +2,85 @@ package com.example.groupstudyingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
 
+import com.example.flatdialoglibrary.dialog.FlatDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class OpeningScreenActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 2000;
     AppData appData;
     FirebaseAuth firebaseAuth;
+    private BroadcastReceiver br;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening_screen);
         getAppData();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // checks if user signed in, if so go to Main else go to login
-                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-                checkLoginStatus(currentUser);
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        setBroadcastReceiver();
     }
 
     private void checkLoginStatus(FirebaseUser currentUser) {
         Intent intent;
         if (currentUser != null) {
             intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+            startActivity(intent);
         } else {
             intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
+            startActivity(intent);
         }
     }
 
     private void getAppData() {
         appData = (AppData) getApplicationContext();
         firebaseAuth = appData.firebaseAuth;
+    }
+
+    private void setBroadcastReceiver() {
+        br = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(FireStoreHandler.LOADING_DATA_SUCCESS)) {
+                    FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                    checkLoginStatus(currentUser);
+                    finish();
+                }
+
+                if (intent.getAction().equals(FireStoreHandler.FAILED_LOADING_DATA)) {
+                    setDataLoadingFailureDialog();
+                }
+            }
+        };
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(FireStoreHandler.LOADING_DATA_SUCCESS);
+        filter.addAction(FireStoreHandler.FAILED_LOADING_DATA);
+        this.registerReceiver(br, filter);
+    }
+
+    private void setDataLoadingFailureDialog() {
+        final FlatDialog flatDialog = new FlatDialog(OpeningScreenActivity.this);
+        flatDialog.setCancelable(false);
+        flatDialog.setTitle("Oops, failure loading data");
+        flatDialog.setSubtitle("Please check your internet connection");
+        flatDialog.setFirstButtonText("Ok got it");
+        flatDialog.withFirstButtonListner(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        flatDialog.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.unregisterReceiver(br);
     }
 }
