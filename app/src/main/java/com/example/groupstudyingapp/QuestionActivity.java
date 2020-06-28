0ef129d7b83097f2584650f82e13a81ba84eba73
Roy Ackerman <roy.akerman@mail.huji.ac.il>
2020-06-28 20:36:32 +0300

    FIX: Show/Hide solution button bug (button disappeared).

--- app/src/main/java/com/example/groupstudyingapp/QuestionActivity.java
+++ app/src/main/java/com/example/groupstudyingapp/QuestionActivity.java
@@ -1,5 +1,5 @@
-package com.example.groupstudyingapp;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -8,8 +8,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.bumptech.glide.Glide;
 import com.hsalf.smileyrating.SmileyRating;
 
 public class QuestionActivity extends AppCompatActivity {
@@ -20,14 +22,42 @@ public class QuestionActivity extends AppCompatActivity {
     private SmileyRating.Type rateType = null;
     private SmileyRating smileyRating;
     private TextView rateText;
+    private ImageView questionImageView;
+    private ImageView solutionImage;
+    private Question question;
+    AppData appData;
+    FireStoreHandler fireStoreHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
-        updateTitleText();
+        loadQuestion();
+        if (question.getRating() > 0) {
+            questionRate = Float.toString(question.getRating());
+        }
         initializeUi();
+        TextView questionTextView = findViewById(R.id.questionTitle);
+        questionTextView.setText(question.getTitle());
+
+        getAppData();
+        fireStoreHandler.setCurrentImagePath(question.getImagePath());
+
+        CircularProgressDrawable circularProgressDrawable = new CircularProgressDrawable(this);
+        circularProgressDrawable.setStrokeWidth(10f);
+        circularProgressDrawable.setCenterRadius(60f);
+        circularProgressDrawable.start();
+
+        Glide.with(this).load(Uri.parse(question.getLink())).placeholder(circularProgressDrawable).into(questionImageView);
+        findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+
+        //TODO - get image uri from firestore (solution)
+        if (question.getAnswers().size() > 0) {
+            solutionImage.setImageURI(Uri.parse(question.getAnswers().get(0).getLink()));
+            // todo show all answers
+        }
         userRateHandler();
+        //TODO - get rate from firestore
         if (rateType != null) {
             updateRate(smileyRating);
         }
@@ -36,16 +66,13 @@ public class QuestionActivity extends AppCompatActivity {
 
     private void initializeUi() {
         final Button solutionButton = findViewById(R.id.solutionButton);
-        final ImageView solutionImage = findViewById(R.id.solutionImage);
-        showSolutionHandler(solutionButton, solutionImage);
         smileyRating = findViewById(R.id.smileyRating);
         rateText = findViewById(R.id.questionRate);
-        rateText.setText("Question Rate: "+questionRate);
+        rateText.setText("Question Rate: " + questionRate);
         Button rateButton = findViewById(R.id.rateButton);
         LottieAnimationView studentAnimation = findViewById(R.id.studentAnimation);
         studentAnimation.setProgress(0);
         studentAnimation.playAnimation();
-
 
         rateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +86,9 @@ public class QuestionActivity extends AppCompatActivity {
                 }
             }
         });
+        questionImageView = findViewById(R.id.questionImage);
+        solutionImage = findViewById(R.id.solutionImage);
+        showSolutionHandler(solutionButton, solutionImage);
     }
 
     private void userRateHandler() {
@@ -67,43 +97,74 @@ public class QuestionActivity extends AppCompatActivity {
             public void onSmileySelected(SmileyRating.Type type) {
                 //TODO - save rate type and rate num to firestore
                 rateType = type;
+                int numOfRates;
                 switch (rateType) {
                     case TERRIBLE:
                         questionRate = "TERRIBLE";
+                        numOfRates = question.getNumOfRates();
+                        question.setRating((numOfRates * question.getRating() + 1) /
+                                (numOfRates + 1));
+                        question.setNumOfRates(numOfRates + 1);
                         break;
-
                     case BAD:
                         questionRate = "BAD";
+                        numOfRates = question.getNumOfRates();
+                        question.setRating((numOfRates * question.getRating() + 2) /
+                                (numOfRates + 1));
+                        question.setNumOfRates(numOfRates + 1);
                         break;
                     case OKAY:
                         questionRate = "OKAY";
+                        numOfRates = question.getNumOfRates();
+                        question.setRating((numOfRates * question.getRating() + 3) /
+                                (numOfRates + 1));
+                        question.setNumOfRates(numOfRates + 1);
                         break;
                     case GOOD:
                         questionRate = "GOOD";
+                        numOfRates = question.getNumOfRates();
+                        question.setRating((numOfRates * question.getRating() + 4) /
+                                (numOfRates + 1));
+                        question.setNumOfRates(numOfRates + 1);
                         break;
                     case GREAT:
                         questionRate = "GREAT";
+                        numOfRates = question.getNumOfRates();
+                        question.setRating((numOfRates * question.getRating() + 5) /
+                                (numOfRates + 1));
+                        question.setNumOfRates(numOfRates + 1);
                         break;
                 }
-                rateText.setText("Question Rate: "+questionRate);
+                rateText.setText("Question Rate: " + question.getRating());
+                fireStoreHandler.updateQuestion(question);
             }
         });
     }
 
     private void updateRate(final SmileyRating smileyRating) {
+        final int deleyMilisec = 500;
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 smileyRating.setRating(rateType, true);
             }
-        }, 500);
+        }, deleyMilisec);
     }
 
-    private void updateTitleText() {
-        String questionTitle = getIntent().getStringExtra("EXTRA_SESSION_ID"); //change to question id num in firestore
-        TextView questionTextView = findViewById(R.id.questionTitle);
-        questionTextView.setText("Question " + questionTitle);
+    private void loadQuestion() {
+        Intent intent = this.getIntent();
+        question = (Question) intent.getSerializableExtra("EXTRA_SESSION_ID");
+    }
+
+    private void hideSolution(final Button solutionButton, final ImageView solutionImage){
+        solutionImage.setVisibility(View.VISIBLE);
+        solutionButton.setText("Hide solution");
+    }
+
+    private void showSolution(final Button solutionButton, final ImageView solutionImage){
+        solutionImage.setVisibility(View.INVISIBLE);
+        solutionButton.setText("Show solution");
     }
 
     private void showSolutionHandler(final Button solutionButton, final ImageView solutionImage) {
@@ -111,15 +172,18 @@ public class QuestionActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (hiddenSolution) {
-                    solutionImage.setVisibility(View.VISIBLE);
-                    solutionButton.setText("Hide solution");
+                    hideSolution(solutionButton, solutionImage);
                     hiddenSolution = false;
                 } else {
-                    solutionImage.setVisibility(View.INVISIBLE);
-                    solutionButton.setText("Show solution");
+                    showSolution(solutionButton, solutionImage);
                     hiddenSolution = true;
                 }
             }
         });
+    }
+
+    private void getAppData() {
+        appData = (AppData) getApplicationContext();
+        fireStoreHandler = appData.fireStoreHandler;
     }
 }
